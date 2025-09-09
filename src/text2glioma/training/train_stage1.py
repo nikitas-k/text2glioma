30096eb8cf1c42695e6bdfbb9d887b460f0850b9
nikitas-k <nikitas-k@github.com>
2025-09-09 22:50:59 +1000

    s

--- src/text2glioma/training/train_stage1.py
+++ src/text2glioma/training/train_stage1.py
@@ -20,7 +20,7 @@ warnings.filterwarnings("ignore")
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("data", type=str, required=True, help="Path to the data JSON file.")
+    parser.add_argument("data", type=str, help="Path to the data JSON file.")
     parser.add_argument("--config", type=str, required=True, help="Path to the config file.")
     parser.add_argument("--run_dir", type=str, required=True, help="Directory containing model checkpoints and logs.")
     parser.add_argument("--device", type=str, default="cuda", help="Device to use for training.")
@@ -39,7 +39,7 @@ def parse_args():
 
 def main():
     args = parse_args()
-    
+
     set_determinism(args.seed)
     print_config()
 
