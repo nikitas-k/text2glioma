b9d0b45fc0f96393b855739f6fad49311d6c43a6
nikitas-k <nikitas-k@github.com>
2025-09-09 21:11:57 +1000

    etc

--- setup.py
+++ setup.py
@@ -11,11 +11,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="nikitas-k.github.io/text2glioma",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     python_requires='>=3.8',
     install_requires=[
         "torch>=1.10.0",
