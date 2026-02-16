c1bd3b61aa7aafc36e368d475c7711467f4a70c2
nikitas-k <nikitas-k@github.com>
2026-02-16 13:31:38 +1100

    f

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setuptools.setup(
     name="text2glioma",
     version="0.2.0",
     include_package_data=True,
-    package_data={"": ["*.json", "*.yaml", "*.nii.gz"]},
+    package_data={
+        "": ["*.json", "*.yaml"],
+        "text2glioma.preprocessing": ["atlas_masks/**/*.nii.gz"],
+    },
 )
