b894ae9f71dd4bd7169bacc6139b81a3857154b7
nikitas-k <nikitas-k@github.com>
2026-08-18 15:04:14 +1000

    big removal

--- scripts/launch_pixel_inpainting_ddp.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-"""Thin launcher for torchrun_nccl.sh compatibility (matches launch_inpainting_ddp.py)."""
-from text2glioma.inpainting.train_pixel_inpainting_ddp import main
-
-if __name__ == "__main__":
-    main()
--- scripts/launch_stage1_ddp.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-"""Thin launcher for torchrun_nccl.sh compatibility."""
-from text2glioma.training.train_stage1_ddp import main
-
-if __name__ == "__main__":
-    main()
